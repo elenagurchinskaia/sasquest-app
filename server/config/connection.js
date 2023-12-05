91f7b73446b34e6462599de785762d06408e0bef
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-12-05 17:40:37 -0600

    updated mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sasquest"
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sasquest"
+// );
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
 
 module.exports = mongoose.connection;
