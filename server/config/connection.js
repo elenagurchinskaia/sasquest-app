7e94997edee28cc32a407ce9b12bcb23c12bb017
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-12-09 00:25:40 -0600

    updated mongodb connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sasquest"
+  process.env.MONGODB_URI ||
+    "mongodb+srv://elena:Texas0905%2A@sasquest.9bxy1tc.mongodb.net/sasquest"
 );
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sasquest"
+// );
+
+// ("mongodb+srv://elena:Texas0905%2A@sasquest.9bxy1tc.mongodb.net");
 
 // mongoose
 //   .connect(process.env.MONGODB_URI, {
